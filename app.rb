9ba9a7f2f5b6065df1d9a9b158bbf957bd51ffd4
HoLoD Sergey <holod@iholod.ru>
2016-03-30 02:41:09 +0300

    Users view, Visit view, small code changes

--- app.rb
+++ app.rb
@@ -14,7 +14,52 @@ class Barber < ActiveRecord::Base
 end
 
 get '/' do
-	@barbers = Barber.all
-	#@barbers = Barber.order "created_at DESC" сортировка по дате создания
 	erb :index	
 end
+
+get '/visit' do
+	@barbers = Barber.all
+	erb :visit
+end
+
+post '/visit' do
+	@barbers = Barber.all
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datestamp = params[:datestamp]
+	@color = params[:color]
+	@barber = params[:barber]
+	
+	if @phone != '' && @username != '' && @datestamp != ''
+		Client.create :name => @username, :phone => @phone, :datestamp => @datestamp, :color => @color, :barber => @barber
+	end
+		
+	hh = { :username => 'Enter your name',
+			:phone => 'Enter your phone', 
+			:datestamp => 'Enter date and time'
+		 }
+		 
+	hh.each do |key, value|
+	
+		if params[key] == ''
+			@error = hh[key]
+			return erb :visit
+		end
+	end
+	
+	erb "Thank you, #{@username}. We call you later!"
+end
+
+get '/users' do
+
+	@show = Client.order "created_at DESC"
+	erb :users
+end
+
+
+
+
+
+
+
